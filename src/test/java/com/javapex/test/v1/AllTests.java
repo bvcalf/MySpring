6a6c698dbe8e2383ea0cdb5406759d1cdf43770d
bvcalf <bvcalf@users.noreply.github.com>
2018-06-21 08:23:19 +0800

    新增Resources接口

--- src/test/java/com/javapex/test/v1/AllTests.java
+++ src/test/java/com/javapex/test/v1/AllTests.java
@@ -4,7 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ApplicationContextTest.class, BeanFactoryTest.class })
+@Suite.SuiteClasses({   ApplicationContextTest.class,
+                        BeanFactoryTest.class,
+                        ResourceTest.class })
 public class AllTests {
 
 }
